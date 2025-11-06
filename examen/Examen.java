c7b93c763f899c1d9a7230e07ea1637379210f26
dilshotV <dilshotvargas@gmail.com>
2025-11-05 19:05:18 -0600

    Update Examen.java

--- examen/Examen.java
+++ examen/Examen.java
@@ -17,32 +17,7 @@ public class Examen {
     public static void main(String[] args) {
 
 
-        String nombre = JOptionPane.showInputDialog("Nombre del vendedor:");
-        String cedula = JOptionPane.showInputDialog("Cédula del vendedor:");
-        String codigo = JOptionPane.showInputDialog("Código del vendedor:");
-        String sucursal = JOptionPane.showInputDialog("Sucursal:");
-        int tieneVehiculo = JOptionPane.showConfirmDialog(null, "¿Tiene vehículo propio?", "Vehículo", JOptionPane.YES_NO_OPTION);
-
-        Vendedor vendedor = new Vendedor(nombre, cedula, codigo, sucursal, tieneVehiculo == JOptionPane.YES_OPTION);
-
-        while (true) {
-            String nombreCliente = JOptionPane.showInputDialog("Nombre del cliente:");
-            String cedulaCliente = JOptionPane.showInputDialog("Cédula del cliente:");
-            String codFactura = JOptionPane.showInputDialog("Código de factura:");
-            double monto = Double.parseDouble(JOptionPane.showInputDialog("Monto de la factura:"));
-            int mes = Integer.parseInt(JOptionPane.showInputDialog("Mes (1-12):"));
-            int electrico = Integer.parseInt(JOptionPane.showInputDialog("Cantidad de productos eléctricos:"));
-            int auto = Integer.parseInt(JOptionPane.showInputDialog("Cantidad de productos automotrices:"));
-            int construccion = Integer.parseInt(JOptionPane.showInputDialog("Cantidad de productos construcción:"));
-
-            Factura f = new Factura(nombreCliente, cedulaCliente, codFactura, electrico, auto, construccion, monto, mes);
-                    vendedor.agregarFactura(f);
-
-            int continuar = JOptionPane.showConfirmDialog(null, "¿Desea ingresar otra factura?", "Continuar", JOptionPane.YES_NO_OPTION);
-            if (continuar != JOptionPane.YES_OPTION) break;
-        }
-
-        JOptionPane.showMessageDialog(null, vendedor.resumen());
+       
     }
 }
        
@@ -60,5 +35,6 @@ public class Examen {
         
         
     
+
 
     
